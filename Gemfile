523bd8f1d8164b31abde9f961f4d815e888a2d3a
Thom Tiber <lakhdar.ma@gmail.com>
2021-04-28 20:49:27 +0200

    #first commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+ruby '2.7.1'
+gem 'dotenv'
+gem 'pry'
+gem 'rspec'
+gem 'rubocop'
+gem 'json'
+gem 'google_drive'
+gem 'googleauth'
+gem 'csv'
+gem 'nokogiri'
+gem 'open-uri'
---
+++ lib/app/scrapping.rb
@@ -0,0 +1,104 @@
+class Scrapping
+    #attr_accessor :
+
+    def initialize
+        @email_url = 'https://www.annuaire-des-mairies.com/'
+        @towns = get_connexion_str('https://www.annuaire-des-mairies.com/val-d-oise.html')
+    end
+
+    #JSON
+    def save_as_JSON(file_name)
+        temp_hash = get_townhall_urls()
+
+        File.new "db/#{file_name}","w"
+        File.open("db/#{file_name}","w") do |f|
+            f.write(JSON.pretty_generate(temp_hash))
+        end
+    end
+
+    #CSV
+    def save_as_csv(file_name)
+        temp_hash = get_townhall_urls()
+        head = {"Name" => "Email"}
+
+        File.new "db/#{file_name}","w"
+        res = CSV.generate(:col_sep => ",") do |csv|
+            temp_hash.each.with_index do |h,i|
+                #csv << head if i == 0
+                csv << h.keys + h.values
+            end
+        end
+        res = "Name,Email,\n#{res}"
+
+        # res = CSV.open("db/#{file_name}", "wb") do |csv|
+        #     csv << temp_hash.first.keys
+        #     temp_hash.each do |the_hash|
+        #         csv << the_hash.values
+        #     end
+        # end
+
+        File.write("db/#{file_name}", res)
+    end
+
+    #Google spreadsheets
+    def save_as_spreadsheet(file_url)
+        return false unless file_url.is_a? String
+        temp_hash = get_townhall_urls()
+
+        ws = connection
+        ws = ws.spreadsheet_by_key(file_url).worksheets[0] #read
+
+        ws[1, 1] = "Name"
+        ws[1, 2] = "Email"
+
+        row = 2
+        temp_hash.each do |h|
+            ws[row, 1] = h.keys.inspect.gsub(/\[|\"\]/, "").gsub(/\"/, "")
+            ws[row, 2] = h.values.gsub(/\[|\"\]/, "").gsub(/\"/, "")
+            row += 1
+        end
+
+        ws.save
+        ws.reload
+    end
+
+    #Google spreadsheets : Connexion
+    def connection
+        return session = GoogleDrive::Session.from_config("config.json")
+    end
+
+
+    def get_connexion_str(url)
+        begin
+            client = Nokogiri::HTML(URI.open(url))
+        rescue => e
+            puts "Exception Message: #{ e.message }"
+        end
+        #puts client
+        return client
+    end
+
+    def get_townhall_email(url)
+        townhall_email_value = url.css('main section[2] div table tbody tr[4] td[2]').text.downcase
+        return false if townhall_email_value.length < 1
+        #puts townhall_email_value
+        return townhall_email_value
+    end
+
+    def get_townhall_urls()
+        townhalls_row = @towns.css('a.lientxt')
+        return false if townhalls_row.length < 1
+        links = []
+        townhalls_row.each.with_index { |n,i|
+            url_end = n["href"]
+            url_end = url_end.slice!(1..url_end.length - 1) if url_end.slice!(0) == '.'
+            url_town = "#{@email_url}#{url_end}"
+            connexion = get_connexion_str(url_town)
+            email = get_townhall_email(connexion)
+            #Hash incrementation
+            links[i] = {n.text.upcase => email}
+        }
+        #puts links
+        return links
+    end
+end

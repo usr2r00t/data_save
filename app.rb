aa65ce63ca20897d20bef7eadb9b1351a0c85ebd
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-29 10:38:28 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -11,16 +11,6 @@ require 'app/scrapping'
 def perform
     scrapping = Scrapping.new()
 
-    #0/ Launch scrapping
-    #scrapping.get_townhall_urls
-
-    #1/ Save in JSON file
-    #scrapping.save_as_JSON("emails.json")
-
-    #2/ Save in Google Spreadsheet file
-    #scrapping.save_as_spreadsheet(("1S47m351U2pWWFhLN4wEXCmbTzhtguH6mqoNgGpoWr-k"))
-
-    #3/Save in CSV file
     scrapping.save_as_csv("emails.csv")
 end
 
